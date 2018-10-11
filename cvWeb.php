83d36ad80f0cf04323329d8ea0af9cea96bf8fd7
glodie <glodie.tshimini@gmail.com>
2018-10-12 00:08:03 +0200

    initial commit

---
+++ cvWeb.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+	<head>
+		<meta charset="utf-8">
+		<title>Mon CV Développeur Logiciel</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/responsive.css"/>
+		<link href="https://fonts.googleapis.com/css?family=El+Messiri" rel="stylesheet">
+		<link rel="stylesheet" href="css/cvWeb.css"/>
+		<link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<main>
+			<section>
+				<div id="leftbar"><!-- identity-->
+					<img id="photo_cv" src="images/cv/photo_cv.jpg" alt="ma photo de presentation" title="Ma photo de presentation"/>
+					<p id="backHome"><a href="index.php">Accueil</a></p>
+					<ul id="presentation">
+						<li id="print"><a href="cv_print.pdf" target="_blank"><img src="images/printer.png" alt="logo_printer"/>Version imprimable</a></li>
+						<li><h4>Glodie Tshimini</h4></li>
+							<ul>
+								<li>j'ai 25 ans et réside à Neuilly/Marne</li>
+								<li>(93330)</li>
+								<li>permis B et véhiculé</li>
+							</ul>
+						<li><a href="contact_email.php"><img src="images/mail.png" alt="mail_logo"/>Me contacter par mail</a></li>
+						<li id='print'><a href="https://www.linkedin.com/in/glodie-tshimini-532836135?trk=hp-identity-name" target="_blank"><img src='images/linkedin.png' alt='lien vers mon profil linkedin'/>Mon profil Linkedin</a></li>
+						<!--
+						<li>
+							<h4>Mes compétences techniques</h4>
+							<ul>
+								<li>Maitrise Html5</li>
+								<li>Maitrise Css3</li>
+								<li>Bases acquises Javascript</li>
+								<li>Bases solides Php/MySql</li>
+								<li>Bonne maitrise Wamp</li>
+								<li>Bases Photoshop</li>
+								<li>Bootstrap en cours</li>
+								<li>Wordpress en cours</li>
+								<li>Referencement en cours</li>
+							</ul>
+						</li>
+						-->
+						<li>
+							<h4>Ma personnalité</h4>
+							<ul>
+								<li>Sérieux</li>
+								<li>Autonome</li>
+								<li>Polyvalent</li>
+								<li>Facilité d'adaptation</li>
+								<li>Facilité de compréhension</li>
+								<li>Autodidacte & Pédagogue</li>
+								<li>Sociable</li>
+								<li>Bon vivant & Curieux</li>
+							</ul>
+						</li>
+						<li>
+							<h4>Langues</h4></li>
+							<ul>
+								<li>Anglais</li>
+								<li><em>Niveau Intermédiaire</em></li><br/>
+								<li>Espagnol</li>
+								<li><em>Niveau Baccalauréat</em></li>
+							</ul>
+						</li>
+						<!--div class="ubloc2 u2">
+							<h5>Logiciels Optique</h5>
+							<ul>
+								<li>Cosium</li>
+								<li>winoptics</li>
+								<li>Synergie</li>
+								<li>Myeasy</li>
+							</ul>
+						</div-->
+						<li>
+							<h4>Bureautique</h4>
+							<ul>
+								<li>Word</li>
+								<li>PowerPoint</li>
+								<li>Outlook</li>
+								<li>Apache OpenOffice</li>
+								<li>Google Docs</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<section>
+				<div id="globalBlock"><!--This Div contains the block competences+Experiences+formations-->
+					<h3 id="title"><mark>RECHERCHE UN STAGE NON REMUNERE DU 03 JUILLET AU 30 AOUT 2017</mark><br/>CONCEPTEUR DEVELOPPEUR INFORMATIQUE NIVEAU II</h3>
+					<article id="techniques">
+						<h4 class="style1">Mes competences techniques</h4>
+						<aside id='langages'>
+							<figure>
+								<img src='images/cv/html5.png' alt='Html5_logo' title='HTML5'/>
+							</figure>
+							<figure>
+								<img src='images/cv/css3.png' alt='Css3_logo' title='CSS3'/>
+							</figure>
+							<figure>
+								<img src='images/cv/javascript.jpg' alt='Javascript_logo' title='JAVASCRIPT'/>
+							</figure>
+							<figure>
+								<img src='images/cv/jquery2.jpg' alt='Jquery_logo' title='Jquery'/>
+							</figure>
+							<!--figure>
+								<img src='images/cv/angular2.jpg' alt='Angular_logo' title='Angular'/>
+							</figure-->
+							<figure>
+								<img src='images/cv/php_color.png' alt='Php_logo' title='PHP'/>
+							</figure>
+							<figure>
+								<img src='images/cv/java.png' alt='Java_logo' title='Java'/>
+							</figure>
+							<figure>
+								<img src='images/cv/oracle.png' alt='Logo_Oracle' title='Oracle'/>
+							</figure>
+							<figure>
+								<img src='images/cv/mysql.png' alt='Mysql_logo' title='MYSQL'/>
+							</figure>
+							<figure>
+								<img src='images/cv/bootstrap.jpg' alt='Bootstrap_logo' title='BOOTSTRAP'/>
+							</figure>
+							<figure>
+								<img src='images/cv/seo.png' alt='SEO_logo' title='SEO'/>
+							</figure>
+							<figure>
+								<img src='images/cv/mockup.png' alt='Mockup_logo' title='Mockup'/>
+							</figure>
+						</aside>
+						<aside id='ccp'>
+							<h4>COMPETENCES</h4>
+							<ul>
+							<details>
+								<summary class='skills'>Composants d'interface</summary>
+								<li>Maqueter une application</li>
+								<li>Développer une interface utilisateur</li>
+								<li>Développer des pages web en lien avec une base de données</li>
+							</details>
+							</ul>
+							<ul>
+								<details>
+								<summary class='skills'>Persistante des données</summary>
+								<li>Concevoir, mettre en place une base de donnée</li>
+								<li>Développer des composants dans le langage de base de données</li>
+								<li>Utilisation de l'Anglais dans le milieu professionnel</li>
+								</details>
+							</ul>
+							<ul>
+							<details>
+								<summary class='skills'>Application N tiers</summary>
+								<li>Concevoir, développer une application de mobilité numérique</li>
+								<li>préparer et éxecuter les plans de tests et le déploiement d'une application</li>
+								<li>Collaborer à la gestion d'un projet informatique</li>
+							</details>
+							</ul>
+						</aside>
+					</article>
+					<article id = "diplomes"><!--DIPLOMAS-->
+						<h4 class="style1">Formations</h4>
+						<div class="bloc1">
+							<div class="ubloc1 it1">
+								<h5>2017</h5>
+								<ul>
+									<li class="color">Diplôme en cours</li>
+									<li><abbr title="Concepteur Développeur Informatique">Concept. Dév. Info.</abbr></li>
+									<li>Greta MTE 93</li>
+									<li>Montreuil</li>
+								</ul>
+							</div>
+							<div class="ubloc1 it2">
+								<h5>2016</h5>
+								<ul>
+									<li class='color'>Formation 8 modules</li>
+									<li>Création d'entreprise</li>
+									<li>BGE Adil</li>
+									<li>Paris</li>
+								</ul>
+							</div>
+							<div class="ubloc1 it3">
+								<h5>2013</h5>
+								<ul>
+									<li class='color'>BTS</li>
+									<li>Optique Lunetterie</li>
+									<li>Lycée Fresnel</li>
+									<li>Paris</li>
+								</ul>
+							</div>
+							<div class="ubloc1 it4">
+								<h5>2010</h5>
+								<ul>
+									<li class='color'>Baccalaureat</li>
+									<li>Scientifique</li>
+									<li>Lycée Cugnot</li>
+									<li>Neuilly sur marne</li>
+								</ul>
+							</div>
+							<!--div class="ubloc1 it5">
+								<h5>2011</h5>
+								<ul>
+									<li class='color'>Validation</li>
+									<li><em>L1 Biologie</em></li>
+									<li>Univ. Paris 13</li>
+									<li>Bobigny</li>
+								</ul>
+							</div-->
+						</div>
+					</article>
+					<article id="expPro">
+						<h4 class="style1">Experiences professionnelles</h4><!--Experiences-->
+						<div class="bloc1">
+							<div class="ubloc1 u1">
+								<h5>Janv - Avr 2016</h5>
+								<ul>
+									<li class='color'>Optical center</li>
+									<li>Opticien diplomé</li>
+									<li>CDD 4 mois</li>
+									<li>Aulnay sous bois</li>
+								</ul>
+							</div>
+							<div class="ubloc1 u2">
+								<h5>Avr 2014 - Mai 2015</h5>
+								<ul>
+									<li class='color'>Optical center</li>
+									<li>Opticien diplomé</li>
+									<li>CDI 13 mois</li>
+									<li>Villiers sur marne</li>
+								</ul>
+							</div>
+							<div class="ubloc1 u3">
+								<h5>Sept 2013 - Mars 2014</h5>
+								<ul>
+									<li class='color'>Générale d'Optique</li>
+									<li>Opticien diplomé</li>
+									<li>CDI 7 mois</li>
+									<li>Noisy le Grand</li>
+								</ul>
+							</div>
+							<div class="ubloc1 u4">
+								<h5>Juin 2011 - Juin 2013</h5>
+								<ul>
+									<li class='color'>Eurest</li>
+									<li>Vendeur-Manager <abbr title="Restauration Rapide">Rest.</abbr></li>
+									<li>Interim 2 ans</li>
+									<li>Stade de France</li>
+								</ul>
+							</div>
+							<div class="ubloc1 u5">
+								<h5>Juin - Aout 2011</h5>
+								<ul>
+									<li class='color'>Adven Express</li>
+									<li>Hôte street marketing</li>
+									<li>CDD 2 mois</li>
+									<li>Paris 16</li>
+								</ul>
+							</div>
+							<div class="ubloc1 u6">
+								<h5>Aout - Sept 2010</h5>
+								<ul>
+									<li class='color'>Auchan</li>
+									<li>Employé Libre Service</li>
+									<li>CDD 6 semaines</li>
+									<li>Neuilly sur marne</li>
+								</ul>
+							</div>
+							<div class="ubloc1 u7">
+								<h5>Juillet 2010</h5>
+								<ul>
+									<li class='color'>Super U</li>
+									<li>Employé Libre Service</li>
+									<li>CDD 4 semaines</li>
+									<li>Noisiel</li>
+								</ul>
+							</div>
+						</div>
+					</article>
+					<article id="hobbies"><!-- HOBBIES-->
+						<h4 class="style1">Centres d'interets</h4>
+						<div id="bloc2">
+							<figure>
+								<img src='images/cv/gym.png' alt='logo_musique' title='MUSCULATION'/>
+							</figure>
+							<figure>
+								<img src='images/cv/travel.png' alt='logo_musique' title='VOYAGER'/>
+							</figure>
+							<figure>
+								<img src='images/cv/football.png' alt='logo_musique' title='FOOTBALL'/>
+							</figure>
+							<figure>
+								<img src='images/cv/coding.png' alt='logo_musique' title='CODER'/>
+							</figure>
+							<figure>
+								<img src='images/cv/bongos.png' alt='logo_musique' title='INSTRUMENT A PERCUSSION'/>
+							</figure>
+							<figure>
+								<img src='images/cv/musique.png' alt='logo_musique' title='MUSIQUE'/>
+							</figure>
+						</div>
+						<div id='flaticon'>
+							<small>
+								Icons made by <a href="http://www.freepik.com" target=_blank title="Freepik">Freepik</a> from <a href="http://www.flaticon.com"  target=_blank title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+							</small>
+						</div>
+					</article>
+				</div><!--CLOSE THE DIV GlobalBlock-->
+			</section>
+		</main>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	    <script type="text/javascript" src="javascript/fond.js"></script>
+	    <script type="text/javascript">
+	    	$(document).ready(function(){
+	    		$('#ccp,#techniques,#diplomes,#expPro,#hobbies').draggable();
+	    	});
+	    </script>
+	</body>
+</html>
